cd87243947a4c664facb53ce7429aeb6276dad53
unknown <peter.mueller1994@gmail.com>
2022-05-27 19:36:08 +0200

    added analysis for food and drink

--- Activity.R
+++ Activity.R
@@ -21,6 +21,9 @@ data <- rbind(data,data4)#adjust if you have more experiments
 data <- rbind(data,data5)#adjust if you have more experiments
 data$Box<-as.factor(data$Box)
 
+#delete empty boxes
+data<-data[!(data$Box %in% c("5_220408","2_220516")),]
+
 #set groups
 Group1 <- c("3_220401","4_220401","5_220401","7_220401","2_220404","3_220404","6_220404","1_220408","4_220408","2_220513","3_220513","6_220513","7_220513","3_220516","5_220516","6_220516")#adjust your groups
 group_bin<-as.integer(data$Box %in% Group1)+1
@@ -84,7 +87,7 @@ act.mdl<-lm(median_activity ~ group*cos(2*pi*time_mdl/24),data=bin)#this is a li
 summary(act.mdl)
 vis<-visreg(act.mdl,"time_mdl",by="group",overlay=TRUE,ylim=c(0,400))
 ##Plot
-ggplot(filter(vis$fit, group == 1), aes(time_mdl, visregFit))+
+a<-ggplot(filter(vis$fit, group == 1), aes(time_mdl, visregFit))+
   geom_boxplot(data=filter(vis$res, group == 1), aes(time_mdl, visregRes,group=as.factor(time_mdl)),position = position_nudge(x=-0.1),fill="deepskyblue",outlier.shape = NA)+
   geom_boxplot(data=filter(vis$res, group == 2), aes(time_mdl, visregRes,group=as.factor(time_mdl)),position = position_nudge(x=0.1),fill="sienna1",outlier.shape = NA)+
   #geom_point(data=filter(vis$res, group == 1), aes(time_mdl, visregRes,group=as.factor(time_mdl)), size=0.5, alpha=.3, position=position_jitter(0.4), colour='blue')+
@@ -94,60 +97,36 @@ ggplot(filter(vis$fit, group == 1), aes(time_mdl, visregFit))+
   geom_line(data=filter(vis$fit, group == 2),colour='red', size=1)+
   geom_ribbon(data=filter(vis$fit, group == 2),aes(ymin=visregLwr, ymax=visregUpr), fill='red',alpha=.3)+
   ylim(-10,200)
-  
-#ggsave(file="median_activity_exclude8h.pdf", dpi=300, scale= 3)
 
-#Violin Plot of all activity
-ggplot(data, aes(y=DistD, x=group, fill=group, group=group))+
-  geom_jitter(shape=2, size=0.005, position=position_jitter(0.2))+
-  geom_violin()#+
-  #scale_y_continuous(trans='log10')
-#ggsave(file="all_activity.pdf", dpi=300)
-  
-## Summarizes data.
-## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
-##   data: a data frame.
-##   measurevar: the name of a column that contains the variable to be summariezed
-##   groupvars: a vector containing names of columns that contain grouping variables
-##   na.rm: a boolean that indicates whether to ignore NA's
-##   conf.interval: the percent range of the confidence interval (default is 95%)
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                 .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
-                     median = median   (xx[[col]], na.rm=na.rm),
-                     sd   = sd     (xx[[col]], na.rm=na.rm)
-                   )
-                 },
-                 measurevar
-  )
-  # Rename the "mean" column    
-  datac <- rename(datac, c("median" = measurevar))
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval: 
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
-  return(datac)
-}
-#any_activity
-binplot <- summarySE(bin, measurevar="any_activity", groupvars=c("time_disp","group"))
-ggplot(binplot, aes(x=time_disp, y=any_activity, fill=group)) + 
-  geom_bar(stat = "identity", position = position_dodge(1))+
-  geom_errorbar(aes(ymin=any_activity-se, ymax=any_activity+se), width=.1, position = position_dodge(1))+
-  #geom_ribbon(aes(xmin="01:00:00", xmax="05:00:00"), alpha=0.5) +
-  coord_polar()
-#ggsave(file="any_activity.pdf", dpi=300)
-  
-  
-  
+#Food and Drink
 
+data.drink<-lapply(as.list(unique(data$Box)), function(x){
+  max(data$Drink[data$Box==x])}  )
+data.drink<-unlist(data.drink)
+data.feed<-lapply(as.list(unique(data$Box)), function(x){
+  max(data$Feed[data$Box==x])}  )  
+data.feed<-unlist(data.feed)
+data.metabolics<-data.frame(
+  drink=data.drink/71*24,
+  feed=data.feed/71*24,
+  group=as.factor(as.integer(unique(data$Box) %in% Group1)+1),
+  box=unique(data$Box))
+data.metabolics$drink[[c(11)]]<-NA #remove false drink values due to bottle emptying on the floor 
+data.metabolics$drink[[c(16)]]<-NA 
+feed<-t.test(feed~group,data=data.metabolics)
+group.col=c("deepskyblue","sienna1")
+d<-ggplot(data.metabolics,aes(x=group,y=feed, fill=group))+
+  geom_boxplot()+
+  scale_fill_manual(values=group.col)+
+  geom_point(position=position_jitter(0.1))+
+  labs(y = "Food Intake per day [g]")+
+  labs(x="")+
+  theme(legend.position = "none")
+drink<-t.test(drink~group,data=data.metabolics)
+f<-ggplot(data.metabolics,aes(x=group,y=drink, fill=group))+
+  geom_boxplot()+
+  scale_fill_manual(values=group.col)+
+  geom_point(position=position_jitter(0.1))+
+  labs(y = "Water Intake per day [ml]")+
+  labs(x="")+
+  theme(legend.position = "none")
