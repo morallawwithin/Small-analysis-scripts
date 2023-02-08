6389aae815ca5bb1d3a742d336245281fcb5ae21
unknown <peter.mueller1994@gmail.com>
2023-02-08 18:22:28 +0100

    added node counting

--- Stainings/Analysis_node_of_Ranvier.R
+++ Stainings/Analysis_node_of_Ranvier.R
@@ -1,17 +1,14 @@
 library(tidyverse)
 library(multimode)
+library(lme4)
+library(lmerTest)
 setwd("D:/Peter/Analysis/KCNA2/P405L_Mice/Caspr1-Kcna2-staining")
 df <-
   list.files(path = "D:/Peter/Analysis/KCNA2/P405L_Mice/Caspr1-Kcna2-staining", pattern = "*.csv") %>% 
   map_df(~read_csv(.))
 df$position<-rep(1:351,length(df$Line_Intensity)/351)/35.1
-df$Line_Intensity
+df$Genotype<-as.factor(df$Genotype)
 df$staining<-as.factor(df$staining)
-ggplot(df_k, aes(position,Line_Intensity, colour=staining))+
-  geom_point()+
-  geom_smooth(method=lm)
-
-
 
 df_k<-filter(df, staining=="anti-kv1.2")
 
@@ -20,15 +17,83 @@ o<-stack(sapply(unique(df_k$imagename),function(x){
 }))
 df_k$Line_Intensity<-o$values
 
-kcna2<-sapply(unique(df$number), function(x){
+#df_k$Line_Intensity<-(df_k$Line_Intensity-mean(df_k$Line_Intensity))/sd(df_k$Line_Intensity)
+
+####Playing
+df_kz<-filter(df_k,Line_Intensity>0)
+df_kz$position<-as.factor(df_kz$position)
+ggplot(filter(df_kz,df_kz$Genotype=="Kcna2 +/+"), aes(position,Line_Intensity))+
+  geom_boxplot(outlier.shape = NA)+
+  scale_y_continuous(limits = quantile(df_kz$Line_Intensity, c(0.1, 0.9)))
+
+  geom_boxplot(data = filter(df_kz,df_kz$Genotype=="Kcna2 +/+"))+
+  geom_boxplot(data = filter(df_kz,df_kz$Genotype=="Kcna2 +/P405L"),colour=blue)
+k<-lmer(Line_Intensity~position+(1|number)+(1|imagename), data=df_k)
+summary(k)
+#####
+
+kcna2<-lapply(unique(df$number), function(x){
 df_knr<-filter(df_k,number==x)
-df_k0nr<-filter(df_k,Line_Intensity>median(df_knr$Line_Intensity))
+df_k0nr<-filter(df_knr,Line_Intensity>0)
 locmodes(df_k0nr$position,mod0=2,display=TRUE)})
-names(kcna2[,1:length(unique(df$number))])<-unique(df$number)
+kcna2_v<-matrix(, nrow = length(unique(df$number)), ncol = 2)
+for (y in c(1:length(unique(df$number)))) {
+  kcna2_v[y,1] <- 5-kcna2[[y]][["locations"]][1]
+  kcna2_v[y,2] <- kcna2[[y]][["locations"]][3]-5 
+}
+kcna2_v
+
+df_k$nodeid<-paste(df_k$imagename,df_k$pointsno, sep = "_")
+
+nod<-sapply(unique(df_k$nodeid), function(x){
+  df_kn<-filter(df_k,nodeid==x)
+  sum(as.numeric(any(df_kn$Line_Intensity>1&df_kn$position<5)),as.numeric(any(df_kn$Line_Intensity>1&df_kn$position>5)))
+})
+nodes<-data.frame("animal"=as.factor(df_k$number[match(unique(df_k$nodeid),df_k$nodeid)]), 
+                  "Genotype"=as.factor(df_k$Genotype[match(unique(df_k$nodeid),df_k$nodeid)]),
+                  "nodes_nr"=nod)
+ggplot(nodes,aes(Genotype,nodes_nr,color=Genotype))+
+  geom_violin()+
+  geom_jitter(height=0)+
+  scale_color_manual(values = c("black","blue")) +
+  ylab("Kv1.2 patches per node")
+
+wilcox.test(nodes_nr~Genotype, data = nodes)
+#plot
+df_kn<-filter(df_k,nodeid=="P405L_391_5.lif - 63x2_1")
+ggplot(df_kn, aes(position,Line_Intensity))+
+  geom_point()+
+  ylim(-3, 5)
+
+len<-t(sapply(unique(df_k$nodeid), function(x){
+  df_kn<-filter(df_k,nodeid==x)
+  c(sum(df_kn$Line_Intensity>1&df_kn$position<5,na.rm=TRUE),sum(df_kn$Line_Intensity>1&df_kn$position>5,na.rm=TRUE))
+
+}))
+len<-c(len[,1],len[,2])/35
+nodelen<-data.frame("animal"=rep(as.factor(df_k$number[match(unique(df_k$nodeid),df_k$nodeid)]),2), 
+                  "Genotype"=rep(as.factor(df_k$Genotype[match(unique(df_k$nodeid),df_k$nodeid)]),2),
+                  "nodes_len"=len)
+ggplot(nodelen,aes(Genotype,nodes_len,color=Genotype))+
+  geom_violin()+
+  geom_jitter(height=0)+
+  scale_color_manual(values = c("black","blue"))+
+  ylab("juxtaparanode length [µm]")
+wilcox.test(nodes_len~Genotype, data = nodelen)
+
 ##################################################################################################################
 df_c<-filter(df, staining=="anti-caspr")
-
-caspr<-sapply(unique(df$number), function(x){
+o<-stack(sapply(unique(df_c$imagename),function(x){
+  (df_c$Line_Intensity[df_c$imagename==x]-mean(df_c$Line_Intensity[df_c$imagename==x]))/sd(df_c$Line_Intensity[df_c$imagename==x])
+}))
+df_c$Line_Intensity<-o$values
+caspr<-lapply(unique(df$number), function(x){
 df_cnr<-filter(df_c,number==x)
-df_c0nr<-filter(df_c,Line_Intensity>median(df_cnr$Line_Intensity))
+df_c0nr<-filter(df_cnr,Line_Intensity>0)
 locmodes(df_c0nr$position,mod0=2,display=TRUE)})
+caspr_v<-matrix(, nrow = length(unique(df$number)), ncol = 2)
+for (y in c(1:length(unique(df$number)))) {
+  caspr_v[y,1] <- 5-caspr[[y]][["locations"]][1]
+  caspr_v[y,2] <- caspr[[y]][["locations"]][3]-5 
+}
+caspr_v
