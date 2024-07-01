d9e8a77e24cac323f9f5d6e057d9c116e4830093
unknown <peter.mueller1994@gmail.com>
2024-07-01 17:59:35 +0200

    added the cells and genotypes

--- Current-Clamp/AP-Analysis.R
+++ Current-Clamp/AP-Analysis.R
@@ -5,37 +5,112 @@ library(ggprism)
 library(pspline)
 setwd("D:/Peter/Analysis/KCNA2/P405L_Mice/E-Phys")
 
-#select the cell
-cell01<-"D:/Peter/Data/KCNA2/P405L_Mice/Ephys/Peter/20240610/24610015.abf"
+####################
+##select the cells
+####################
+directory<-"D:/Peter/Data/KCNA2/P405L_Mice/Ephys/Peter"
+cell01<-c(paste0(directory,
+               "/20240503/24503005",
+               ".abf"),
+          "Kcna2+/+")
+cell02<-c(paste0(directory,
+                 "/20240506/24506009",
+                 ".abf"),
+          "Kcna2+/+")
+cell03<-c(paste0(directory,
+                 "/20240506/24506017",
+                 ".abf"),
+          "Kcna2+/+")
+cell04<-c(paste0(directory,
+                 "/20240506/24506024",
+                 ".abf"),
+          "Kcna2+/+")
+cell05<-c(paste0(directory,
+                 "/20240506/24506032",
+                 ".abf"),
+          "Kcna2+/+")
+cell06<-c(paste0(directory,
+                 "/20240507/24507006",
+                 ".abf"),
+          "Kcna2+/+")
+cell07<-c(paste0(directory,
+                 "/20240510/24510004",
+                 ".abf"),
+          "Kcna2+/+")
+cell08<-c(paste0(directory,
+                 "/20240510/24510017",
+                 ".abf"),
+          "Kcna2+/+")
+cell09<-c(paste0(directory,
+                 "/20240510/24510024",
+                 ".abf"),
+          "Kcna2+/+")
+cell10<-c(paste0(directory,
+                 "/20240604/24604018",
+                 ".abf"),
+          "Kcna2+/+")
+cell11<-c(paste0(directory,
+                 "/20240604/24604033",
+                 ".abf"),
+          "Kcna2+/+")
+cell12<-c(paste0(directory,
+                 "/20240610/24610008",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell13<-c(paste0(directory,
+                 "/20240610/24610015",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell14<-c(paste0(directory,
+                 "/20240611/24611005",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell15<-c(paste0(directory,
+                 "/20240611/24611012",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell16<-c(paste0(directory,
+                 "/20240613/24613003",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell16<-c(paste0(directory,
+                 "/20240614/24613016",
+                 ".abf"),
+          "Kcna2+/P405L")
+cell16<-c(paste0(directory,
+                 "/20240502/24502006",
+                 ".abf"),
+          "Kcna2+/P405L")
 
-
-cells<-list(cell01)#, cell02, cell03, cell04, cell05, cell06, cell07, cell08)
-cellname<-c("cell1","cell2","cell3","cell4", "cell5", "cell6", "cell7", "cell8")
+cells<-list(cell01, cell02, cell03, cell04, cell05, cell06, cell07, cell08, cell09, cell10, 
+            cell11, cell12, cell13, cell14, cell15, cell16)
+cellname<-c(paste0("cell0",c(1:9)),paste0("cell",c(10:16)))
 
 sweep<-data.frame(matrix(ncol = 4, nrow = 0))
-colnames(sweep)<-c("cell","current","AP")
+colnames(sweep)<-c("cell","genotype","current","AP")
 
-AP_properties<-data.frame(matrix(ncol = 4, nrow = 0))
-colnames(AP_properties)<-c("cell","current","AP_Nr","Threshold")
+AP_properties<-data.frame(matrix(ncol = 5, nrow = 0))
+colnames(AP_properties)<-c("cell","genotype","current","AP_Nr","Threshold")
 
-AP_IFF<-data.frame(matrix(ncol = 4, nrow = 0))
-colnames(AP_IFF)<-c("cell","current","AP_Nr","IFF")
+AP_IFF<-data.frame(matrix(ncol = 5, nrow = 0))
+colnames(AP_IFF)<-c("cell","genotype","current","AP_Nr","IFF")
 
 for ( i in 1:length(cellname)){
 
 #
 curr_cell<-cells[[i]]
-#How many sweeps
-  sweepnr<-17
-  samplerate<-100000 #/s
 #read data  
-  data<-readABF(curr_cell)
+  data<-readABF(curr_cell[1])
+#How many sweeps
+  sweepnr<-length(data$data)
+  samplerate<-100000 #/s
+
 #prepare to store  
   AP_nr<-rep(0, sweepnr)
   AP_thres<-list()
   AP_isi<-list()
 #which voltage at which step  
-  curr<-25*(-4:12)
+  curr<-25*(-4:(sweepnr-5))
 #do the analysis  
 for (ii in 1:sweepnr){
     sweep.data<-as.data.frame(data,sweep=ii)
@@ -67,11 +142,13 @@ for (ii in 1:sweepnr){
       
       
       AP_df<-data.frame('ind'=sweep.data$`Time [s]`[AP_ind],'volt'=sweep.data$`INcc 0 [mV]`[AP_ind])
-      print(ggplot(sweep.data,aes(`Time [s]`,`INcc 0 [mV]`))+
-      geom_line(aes(`Time [s]`,first_derivate), color='grey')+
-      geom_line()+
-      geom_point(data=AP_df,aes(`ind`,volt),color="red")+
-      ylim(c(-80,50)))
+      #print(
+      #ggplot(sweep.data,aes(`Time [s]`,`INcc 0 [mV]`))+
+      #geom_line(aes(`Time [s]`,first_derivate), color='grey')+
+      #geom_line()+
+      #geom_point(data=AP_df,aes(`ind`,volt),color="red")+
+      #ylim(c(-80,50))
+      #)
     }
     else {
       AP_nr[ii]<-0
@@ -84,7 +161,8 @@ for (ii in 1:sweepnr){
 
   sweep<-rbind(sweep,
     data.frame("cell"=rep(cellname[i],sweepnr),
-               "current"=curr, 
+               "genotype"=rep(curr_cell[2],sweepnr),
+               "current"=curr[1:sweepnr], 
                     "AP"= AP_nr))
   
   
@@ -96,6 +174,7 @@ for (ii in 1:sweepnr){
   AP_properties<-rbind(AP_properties,
                        data.frame(
                          "cell"=rep(cellname[i],length(unlist(AP_thres))),
+                         "genotype"=rep(curr_cell[2],length(unlist(AP_thres))),
                          "current"=as.numeric( stringr::str_extract(AP_thres_names,"[123]?[2570][05]")),
                          "AP_Nr"=as.numeric(AP_thres_nr),
                          "Threshold"=unlist(AP_thres)
@@ -108,20 +187,62 @@ for (ii in 1:sweepnr){
   AP_IFF<-rbind(AP_IFF,
                        data.frame(
                          "cell"=rep(cellname[i],length(unlist(AP_isi))),
+                         "genotype"=rep(curr_cell[2],length(unlist(AP_isi))),
                          "current"=as.numeric( stringr::str_extract(AP_isi_names,"[123]?[2570][05]")),
                          "AP_Nr"=as.numeric(AP_isi_nr),
                          "IFF"=unlist(AP_isi)
                        ))
-  
+ print(paste0("finished analysis of ",curr_cell[1])) 
 }    
-ggplot(AP_IFF,aes(AP_Nr,IFF, col=current))+
-       geom_point()+
-       theme_classic()
 
-ggplot(AP_properties,aes(AP_Nr,Threshold, col=current))+
+p1<-ggplot(sweep,aes(current,AP,group=as.factor(genotype), col=as.factor(genotype),fill=as.factor(genotype)))+  
+  stat_summary(fun = mean, 
+               fun.min = function(x) mean(x) - sd(x)/sqrt(length(x)), 
+               fun.max = function(x) mean(x) + sd(x)/sqrt(length(x)),
+               geom = 'errorbar',  width = 30,size=1,  position = position_dodge(width = 0.5)) +
+  stat_summary(fun = mean, fun.min = mean, fun.max = mean,
+               geom = 'path',  size=1, position = position_dodge(width = 0.5), aes(col=as.factor(genotype))) +
+  stat_summary(fun = mean,
+               geom = 'point', size=5, position = position_dodge(width = 0.5),shape=17) +
+  scale_colour_manual(values = c("black", "blue")) +
+  theme_prism(base_size = 14)+
+  xlab("injected current [pA]") + ylab("number of AP")
+
+model_AP<-lm(AP~current*genotype,data= sweep)
+summary(model_AP)
+ggsave(p1,width = 6, height = 4,
+       file="D:/Peter/Analysis/KCNA2/P405L_Mice/E-Phys/APnumber.png")
+
+p2<-ggplot(AP_IFF,aes(as.factor(AP_Nr),IFF, col=genotype))+
+  geom_boxplot()+
+  scale_colour_manual(values = c("black", "blue")) +
+  scale_fill_manual(values = c("white",rgb(191/255,191/255,1,1))) +
+  theme_prism(base_size = 14)+
+  xlab("AP number") + ylab("inst. firing fre. [Hz]")
+ggsave(p2,width = 6, height = 4,
+       file="D:/Peter/Analysis/KCNA2/P405L_Mice/E-Phys/instfiringfreqbox.png")
+
+model_IFF<-lm(IFF~AP_Nr+current+genotype,data= AP_IFF)
+summary(model_IFF)
+
+
+ggplot(AP_properties,aes(AP_Nr,Threshold, col=genotype))+
   geom_point()+
-  theme_classic()
+  scale_colour_manual(values = c("black", "blue")) +
+  theme_prism(base_size = 14)+
+  xlab("AP number") + ylab("inst. firing fre. [Hz]")
   
-write_xlsx(sweep, path=paste0(curr_cell[1],"_Mcurrent.xlsx"))
-  
-  
+
+##Examples
+data1<-readABF(cell01[1])
+data1<-as.data.frame(data1,sweep=13)
+data2<-readABF(cell14[1])
+data2<-as.data.frame(data2,sweep=13)
+p3<-ggplot(data1,aes(`Time [s]`,`INcc 0 [mV]`))+
+  geom_line()+
+  geom_line(data=data2,color="blue")+
+  ylim(c(-80,50))+
+  theme_prism(base_size = 14)+
+  xlab("time [s]") + ylab("memb. pot. [mV]")
+ggsave(p3,
+       file="D:/Peter/Analysis/KCNA2/P405L_Mice/E-Phys/examples_200pA.png")  
