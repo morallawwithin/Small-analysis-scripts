f57f520d6c88c5e12a72664be2c89c577e1eaf81
unknown <peter.mueller1994@gmail.com>
2025-05-29 14:11:12 +0200

    added new analysis (tau)

--- Voltage-Clamp/BMK86-P1/KCNA1_Toxin.R
+++ Voltage-Clamp/BMK86-P1/KCNA1_Toxin.R
@@ -4,6 +4,7 @@ library(writexl)
 library(ggprism)
 library(RColorBrewer)
 library(lsmeans)
+library(minpack.lm)
 condition<-c(
   "base",
   "extra.0",
@@ -72,16 +73,17 @@ cell11<-matrix(cbind(paste("D:/Peter/Data/KCNA2/BMK86/KCNA1 WT/2022_4_8/",
                                       pattern="_00(2[2-9]|3[0-2])"),
                            sep=""),
                      condition ), ncol = 2)
-cell_values<-data.frame(matrix(ncol = 4, nrow = 0))
-colnames(cell_values)<-c("cell","condition","amplitude","v1/2")
-cell_values_act<-data.frame(matrix(ncol = 5, nrow = 0))
-colnames(cell_values_act)<-c("cell","condition","voltage","cond. norm.","tail. curr.")
+cell_values<-data.frame(matrix(ncol = 5, nrow = 0))
+colnames(cell_values)<-c("cell","condition","amplitude","v1/2","tau")
+cell_values_act<-data.frame(matrix(ncol = 6, nrow = 0))
+colnames(cell_values_act) <- c("cell", "condition", "voltage", "cond. norm.", "tail. curr.", "tau")
 cellname<-c(paste("cell0",c(1:9),sep=""),paste("cell",c(10:11),sep=""))
 cells<-list(cell01, cell02, cell03, cell04, cell05, cell06, cell07, cell08, cell09, cell10, cell11)
 for ( i in 1:length(cellname)){
   curr_cell<-cells[[i]]
   cell_values_i<-cbind(rep(cellname[i],nrow(curr_cell)),
                        curr_cell[,2],
+                       rep(0, nrow(curr_cell)),
                        rep(0, nrow(curr_cell)),
                        rep(0, nrow(curr_cell)))
   
@@ -93,7 +95,7 @@ for ( i in 1:length(cellname)){
     tail_curr<-rep(0, sweepnr)
     curr<-rep(0, sweepnr)
     volt<-10*(-8:7)
-    
+    tau <-rep(0, sweepnr)
     for (ii in 1:sweepnr){
       sweep.data<-as.data.frame(data,sweep=ii)
       sweep.max<-sweep.data[c(500:5000),c(1,3)]
@@ -101,17 +103,40 @@ for ( i in 1:length(cellname)){
       cond[ii]<-max(sweep.max/(volt[i]+95))
       sweep.tail<-sweep.data[c(20000:21000),c(1,3)]
       tail_curr[ii]<-min(sweep.tail)
+      # --- Tau of Inactivation ---
+    # Fit decay after peak
+    peak_idx <- which(sweep.data[,3]==max(sweep.max))
+    decay_data <- sweep.data[peak_idx:20000,c(1,3)]
+    #decay_data[,2]<-decay_data[,2]-min(decay_data[,2])
+    colnames(decay_data)<-c("time","current")
+    try({
+      fit <- nlsLM(current ~ A * exp(time / tau) + C,
+                   start = list(A = decay_data$current[1], tau = -0.5, C = min(decay_data$current)),
+                   control = nls.lm.control(maxiter = 500), data=decay_data)
+      tau[ii] <- coef(fit)["tau"]
+        decay_data$fit<-predict(fit)
+    #print(
+      ggplot(decay_data, aes(x = time)) +
+           geom_line(aes(y = current), color = "blue", size = 1, alpha = 0.6) +
+           geom_line(aes(y = fit), color = "red", size = 1) +
+           theme_minimal()
+      #)
+    }, silent = TRUE)
+
+
     }
     cond_norm<-cond/max(cond)
     tail_norm<-tail_curr/min(tail_curr)
-    cell_values_act<-rbind(cell_values_act,
-                           cbind(rep(cellname[i],sweepnr),
-                                 curr_cell[s,2],
-                                 volt,
-                                 cond_norm,
-                                 tail_norm)) 
+    cell_values_act <- rbind(cell_values_act,
+                             cbind(rep(cellname[i], sweepnr),
+                                   curr_cell[s,2],
+                                   volt,
+                                   cond_norm,
+                                   tail_norm,
+                                   tau))
     #cell_values[s,4]<-coef(model)[2]
     cell_values_i[s,3]<-curr[12] #40 mV
+    cell_values_i[s,5]<-tau[12] #40 mV
   }
   
   cell_values<-rbind(cell_values,cell_values_i)
@@ -132,10 +157,11 @@ for ( i in cellname){
 
 cell_values [c('Condition', 'Time')]<- str_split_fixed(cell_values$V2,"\\.",2)
 cell_values<-cell_values[!cell_values$Condition=="base",]
-colnames(cell_values)<-c("cell"  ,      "ident"   ,     "raw.Amp"   ,     "norm.Amp"   ,     "Condition", "Time")
+colnames(cell_values)<-c("cell"  ,      "ident"   ,     "raw.Amp"   ,     "norm.Amp"   , "Tau",    "Condition", "Time")
 cell_values$norm.Amp<-as.numeric(cell_values$norm.Amp)
 cell_values$Time<-as.numeric(cell_values$Time)
-
+cell_values$Tau<-as.numeric(cell_values$Tau)
+cell_values$Tau[cell_values$Tau==0]<-NA
 #write_xlsx(cell_values,"D:/Peter/Analysis/KCNA2/BMK86-P1/V381Y.xlsx")
 
 
@@ -172,6 +198,37 @@ summary <- cell_values %>%
             nAMp = n()) %>%
   mutate(SEMAmp = sdAmp/sqrt(nAMp))
 
+##################
+ggplot(data=cell_values,aes(x=Time, y=abs(Tau), group=Condition, fill=Condition,shape = Condition))+
+  coord_cartesian(clip = 'off',ylim=c(0,4), xlim = c(0,12))+
+  scale_y_continuous(expand = c(0, 0))+
+  scale_x_continuous(expand = c(0, 0))+
+  stat_summary(fun = mean, 
+               fun.min = function(x) mean(x) - sd(x)/sqrt(length(x)), 
+               fun.max = function(x) mean(x) + sd(x)/sqrt(length(x)),
+               geom = 'errorbar',  width = 0.25,  col="black") +
+  stat_summary(fun = mean, fun.min = mean, fun.max = mean,
+               geom = 'path',  size=1,  aes(col=Condition)) +
+  stat_summary(fun = mean, fun.min = mean, fun.max = mean,
+               geom = 'point',  size=4, aes(col=Condition)) +
+  scale_colour_manual(values = c("#3c5396", "#b5595f")) +
+  scale_fill_manual(values = c("#bac5e3", "#e6a2a4")) +
+  scale_shape_manual (values =c(21,22))+
+  ylab(expression('tau '[inactivation]*'[s]'))+
+  theme_prism(base_size = 12)
+
+ggsave(filename = "D:/Peter/Analysis/KCNA2/BMK86-P1/KCNA1_inactivation.svg", width = 3.5, height = 2)
+
+kcna1_tau<-lm(Tau~Condition*Time, data = cell_values)
+anova(kcna1_tau)
+lsmeans(kcna1_tau, pairwise ~ Condition | Time, adjust = "tukey")
+
+summary <- na.omit(cell_values) %>%
+  group_by( Condition, Time) %>% 
+  summarise(meanTau = mean(Tau),
+            sdTau = sd(Tau),
+            nTau = n()) %>%
+  mutate(SEMTau = sdTau/sqrt(nTau))
 #summary(model)
 #sweep$PredictionsNLS <- predict(model)
 #ggplot(data=sweep, aes(x=voltage,y=conductance))+
